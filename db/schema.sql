2ad6baf0fd5443f9ffac915ab16d8950f3664e85
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-03-13 22:40:34 -0400

    slight change in schema layout

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,27 @@ CREATE DATABASE employees_db;
 USE employees_db; 
 
 CREATE TABLE departments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL(10, 2) NOT NULL,
-    department_id INT, 
+    department_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT, 
     manager_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
